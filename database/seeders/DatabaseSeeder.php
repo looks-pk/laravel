b80bf974f45b74217d60d8c69d740c2d13ec3ed7
abdullah <abdullahaqeelpakarab@gmail.com>
2025-05-12 01:32:39 +0500

    app pro

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Create a test user
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        
+        // Create an admin user
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'digitalpartnerabdullah@gmail.com',
+            'password' => Hash::make('digitalpartnerabdullah@gmail.com'),
+            'is_admin' => true,
         ]);
 
         $this->call([

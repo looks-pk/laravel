779d9ccac20ee808abd4dd3c36899b72b23dec4c
jamshaid290-dev <jamshaiddigitalpartner@gmail.com>
2026-08-08 03:49:41 +0500

    Update invisia-serena-seat-pro.blade.php

--- resources/views/products/products/invisia-serena-seat-pro.blade.php
+++ resources/views/products/products/invisia-serena-seat-pro.blade.php
@@ -423,7 +423,7 @@
                     </div>
                     <!-- Image -->
                     <div class="w-full lg:w-1/2">
-                        <img src="{{ asset('al-prod-page-imgs/Rectangle%20320-min.png') }}" alt="Bathroom Safety Solutions"
+                        <img src="{{ asset('al-prod-page-imgs/INV-WSAL-18-PCG.webp') }}" alt="Bathroom Safety Solutions"
                             class="w-full h-full object-cover" style="min-height: 400px;">
                     </div>
                 </div>

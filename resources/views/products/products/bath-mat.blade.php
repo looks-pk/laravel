abdb7a39e4e1c3eaba626b4fbe924137e5c99b7b
jamshaid290-dev <jamshaiddigitalpartner@gmail.com>
2026-08-11 04:18:36 +0500

    Update bath-mat.blade.php

--- resources/views/products/products/bath-mat.blade.php
+++ resources/views/products/products/bath-mat.blade.php
@@ -343,7 +343,7 @@
                     </div>
                     <!-- Image -->
                     <div class="w-full lg:w-1/2">
-                        <img src="{{ asset('al-prod-page-imgs/Rectangle%20320-min.png') }}" alt="Bathroom Safety Solutions"
+                        <img src="{{ asset('al-prod-page-imgs/waterstopper1.webp') }}" alt="Bathroom Safety Solutions"
                             class="w-full h-full object-cover" style="min-height: 400px;">
                     </div>
                 </div>

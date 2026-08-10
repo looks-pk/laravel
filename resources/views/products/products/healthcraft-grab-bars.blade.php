53ad5e8267d6c03a00ae8629cb0d20d423ae978b
jamshaid290-dev <jamshaiddigitalpartner@gmail.com>
2026-08-11 00:31:09 +0500

    Update healthcraft-grab-bars.blade.php

--- resources/views/products/products/healthcraft-grab-bars.blade.php
+++ resources/views/products/products/healthcraft-grab-bars.blade.php
@@ -397,7 +397,7 @@
                                             </div>
                                             <div class="flex justify-between border-b pb-2">
                                                 <span class="font-medium text-gray-700">Supplier:</span>
-                                                <span class="text-gray-600">Home2Stay</span>
+                                                <span class="text-gray-600">Home2stay</span>
                                             </div>
                                         </div>
                                     </div>

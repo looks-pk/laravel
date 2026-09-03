ba9697c7c62bd97f135e8d2c2ed4d0db6186c3b3
jamshaid290-dev <jamshaiddigitalpartner@gmail.com>
2026-09-04 01:51:54 +0500

    Update ramps.blade.php

--- resources/views/products/categories/ramps.blade.php
+++ resources/views/products/categories/ramps.blade.php
@@ -271,7 +271,7 @@
                         <p class="font-bold text-primary mb-5 flex items-center">
                             <!-- <span class="bg-primary/10 text-primary px-3 py-1 rounded-full text-sm">2 Products</span> -->
                         </p>
-                        <a href="/products/folding-&-olling-ramps/"
+                        <a href="/products/folding-&-rolling-ramps/"
                             class="mt-auto w-full bg-primary hover:bg-primary-dark text-white py-3 px-6 rounded-lg transition duration-300 text-center font-medium">
                             View All Products
                         </a>
